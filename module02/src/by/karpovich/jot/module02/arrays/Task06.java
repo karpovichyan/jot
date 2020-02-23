cb5c8e2adba4290ceded7c74545af746a012a4e5
karpovichyan <karpovichjan@gmail.com>
2020-02-23 12:13:00 +0300

    JOT-60. Array task 6 - done

---
+++ module02/src/by/karpovich/jot/module02/arrays/Task06.java
@@ -0,0 +1,29 @@
+package by.karpovich.jot.module02.arrays;
+
+/*
+6. Задана последовательность N вещественных чисел. Вычислить сумму чисел, порядковые номера которых
+   являются простыми числами.
+ */
+public class Task06 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "] - " + arr[i]);
+
+            int countOfDivider = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    countOfDivider++;
+                }
+            }
+            if (countOfDivider == 2) {
+                sum += arr[i];
+            }
+        }
+        System.out.println(" ");
+        System.out.println("sum = " + sum);
+    }
+}
