31c3062214db9834a62c37eba3282654fa36428e
karpovichyan <karpovichjan@gmail.com>
2020-03-04 23:06:48 +0300

    JOT-65 Multidimensional Task01 - done

--- module02/src/by/karpovich/jot/module02/multidimensional/Task01.java
+++ module02/src/by/karpovich/jot/module02/multidimensional/Task01.java
@@ -1,14 +1,31 @@
 package by.karpovich.jot.module02.multidimensional;
 
+import java.util.Random;
+
+/*
+1. Дана матрица. Вывести на экран все нечетные столбцы, у которых первый элемент больше последнего.
+ */
 public class Task01 {
     public static void main(String[] args) {
 
+        int[][] arr = new int[5][5];
 
-        int[][] array = new int[3][5];
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = random.nextInt(10);
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+        System.out.println();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (j % 2 != 0 && (arr[0][j] > arr[arr.length - 1][j])) {
+                    System.out.print(arr[i][j] + " ");
+                }
             }
             System.out.println();
         }
