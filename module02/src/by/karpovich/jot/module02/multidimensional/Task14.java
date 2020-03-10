6d888610d9d1f16b4f94d1aa0023ab2295bfb053
karpovichyan <karpovichjan@gmail.com>
2020-03-10 22:16:06 +0300

    JOT-79 Multidimensional Task14 - done

---
+++ module02/src/by/karpovich/jot/module02/multidimensional/Task14.java
@@ -0,0 +1,30 @@
+package by.karpovich.jot.module02.multidimensional;
+
+/*
+14. Сформировать случайную матрицу m x n, состоящую из нулей и единиц, причем в каждом столбце число
+единиц равно номеру столбца.
+ */
+public class Task14 {
+    public static void main(String[] args) {
+
+        int m = 1 + (int) (Math.random() * 10);
+        int n = 1 + (int) (Math.random() * 10);
+        int[][] arr = new int[m][n];
+        System.out.println("matrix " + m + "x" + n + "\n");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (m < n) {
+                System.out.println("matrix " + m + "x" + n + " is undefined");
+                break;
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+
+                if (i <= j) {
+                    arr[i][j] = 1;
+                }
+                System.out.printf("%-2d", arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
