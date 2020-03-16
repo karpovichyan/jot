f9afed122300bc1e94b2f98d09f049f07951174e
karpovichyan <karpovichjan@gmail.com>
2020-03-16 22:58:50 +0300

    JOT-67 Multidimensional Task03 decomposition

--- module02/src/by/karpovich/jot/module02/multidimensional/Task03.java
+++ module02/src/by/karpovich/jot/module02/multidimensional/Task03.java
@@ -7,35 +7,52 @@ import java.util.Random;
  */
 public class Task03 {
     public static void main(String[] args) {
-        int[][] arr = new int[4][6];
+
+        int[][] arr = initArr(4, 5);
+        printArr(arr);
+        System.out.println();
+        printRow(arr, 2);
+        System.out.println();
+        printColumn(arr, 4);
+    }
+
+    public static int[][] initArr(int m, int n) {
+        int[][] arr = new int[m][n];
         Random random = new Random();
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 arr[i][j] = random.nextInt(10);
+            }
+        }
+        return arr;
+    }
+
+    public static void printArr(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
                 System.out.printf("%-2d", arr[i][j]);
             }
             System.out.println();
         }
-        System.out.println();
-        int k = 3;
-        int p = 2;
+    }
 
+    public static void printRow(int[][] arr, int k) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 if (i == k - 1) {
                     System.out.print(arr[i][j] + " ");
                 }
-
             }
         }
-        System.out.println();
+    }
+
+    public static void printColumn(int[][] arr, int p) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 if (j == p - 1) {
                     System.out.print(arr[i][j] + " ");
                 }
-
             }
         }
     }
