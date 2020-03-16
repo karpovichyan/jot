10f65ad2847fc9beb13da7ff152d6890c44b8c00
karpovichyan <karpovichjan@gmail.com>
2020-03-16 22:59:48 +0300

    JOT-72 Multidimensional Task08 decomposition

--- module02/src/by/karpovich/jot/module02/multidimensional/Task08.java
+++ module02/src/by/karpovich/jot/module02/multidimensional/Task08.java
@@ -10,22 +10,11 @@ import java.util.Scanner;
  */
 public class Task08 {
     public static void main(String[] args) {
-
         int sizeOfRow = 4;
         int sizeOfColumn = 5;
-
-        int[][] arr = new int[sizeOfRow][sizeOfColumn];
+        int[][] arr = initArray(sizeOfRow, sizeOfColumn);
         int[][] tempArr = new int[2][sizeOfRow];
-
-        Random random = new Random();
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = random.nextInt(10);
-                System.out.printf("%-3d", arr[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
+        printArray(arr);
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("enter the number of the first column: ");
@@ -33,6 +22,24 @@ public class Task08 {
         System.out.print("enter the number of the second column: ");
         int secondColumn = Integer.parseInt(scanner.nextLine());
 
+        initTempArray(arr, tempArr, firstColumn, secondColumn);
+        swapColumn(arr, tempArr, firstColumn, secondColumn);
+        printArray(arr);
+    }
+
+    public static int[][] initArray(int m, int n) {
+        int[][] arr = new int[m][n];
+
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = random.nextInt(10);
+            }
+        }
+        return arr;
+    }
+
+    public static void initTempArray(int[][] arr, int[][] tempArr, int firstColumn, int secondColumn) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 if (j == firstColumn - 1) {
@@ -43,16 +50,24 @@ public class Task08 {
                 }
             }
         }
-        System.out.println();
+    }
 
+    public static void swapColumn(int[][] arr, int[][] tempArr, int firstColumn, int secondColumn) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                if (j == firstColumn  - 1) {
+                if (j == firstColumn - 1) {
                     arr[i][j] = tempArr[1][i];
                 }
                 if (j == secondColumn - 1) {
                     arr[i][j] = tempArr[0][i];
                 }
+            }
+        }
+    }
+
+    public static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
                 System.out.printf("%-3d", arr[i][j]);
             }
             System.out.println();
