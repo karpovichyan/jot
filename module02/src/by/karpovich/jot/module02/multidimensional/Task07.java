304d4dfe068053faaa75170461fecf4d0541f76e
karpovichyan <karpovichjan@gmail.com>
2020-03-16 22:59:35 +0300

    JOT-71 Multidimensional Task07 decomposition

--- module02/src/by/karpovich/jot/module02/multidimensional/Task07.java
+++ module02/src/by/karpovich/jot/module02/multidimensional/Task07.java
@@ -9,20 +9,42 @@ package by.karpovich.jot.module02.multidimensional;
  */
 public class Task07 {
     public static void main(String[] args) {
-        double[][] arr = new double[7][7];
+        double[][] arr = initArray(7);
+       printArray(arr);
+       System.out.println("Number of positive elements: " + numberOfPositiveElements(arr));
+    }
 
-        int count = 0;
+    public static double[][] initArray(int n) {
+        double[][] arr = new double[n][n];
+
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 arr[i][j] = Math.sin((double) (i * i - j * j) / arr.length);
-                if (arr[i][j] > 0) {
-                    count++;
-                }
+            }
+        }
+        return arr;
+    }
+
+    public static void printArray(double[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
                 System.out.printf("%.3f\t", arr[i][j]);
             }
             System.out.println();
         }
-        System.out.println("\nnumber of positive elements " + count);
     }
 
+    public static int numberOfPositiveElements(double[][] arr) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i][j] > 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
+
