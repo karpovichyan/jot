bbbd2c34d1d43de439673345c29d8afb33b59451
karpovichyan <karpovichjan@gmail.com>
2020-03-16 22:58:09 +0300

    JOT-66 Multidimensional Task02 decomposition

--- module02/src/by/karpovich/jot/module02/multidimensional/Task02.java
+++ module02/src/by/karpovich/jot/module02/multidimensional/Task02.java
@@ -8,19 +8,36 @@ import java.util.Random;
 
 public class Task02 {
     public static void main(String[] args) {
+        int[][] arr = initArray(5);
+        printArray(arr);
+        System.out.println();
+        printFirstDiag(arr);
+        System.out.println();
+        printSecondDiag(arr);
+    }
 
-        int[][] arr = new int[5][5];
+    public static int[][] initArray(int n) {
+        int[][] arr = new int[n][n];
 
         Random random = new Random();
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 arr[i][j] = random.nextInt(10);
+            }
+        }
+        return arr;
+    }
+
+    public static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
                 System.out.printf("%-2d", arr[i][j]);
             }
             System.out.println();
         }
-        System.out.println();
+    }
 
+    public static void printFirstDiag(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 if (i == j) {
@@ -28,7 +45,9 @@ public class Task02 {
                 }
             }
         }
-        System.out.println();
+    }
+
+    public static void printSecondDiag(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 if (i + j == arr.length - 1) {
