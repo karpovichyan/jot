0991b236635744e50f825cf87860e8f0f69946e4
karpovichyan <karpovichjan@gmail.com>
2020-04-01 21:11:24 +0300

    JOT-102 decomposition Task11 done

---
+++ module02/src/by/karpovich/jot/module02/decomposition/Task11.java
@@ -0,0 +1,32 @@
+package by.karpovich.jot.module02.decomposition;
+
+/*
+11. Написать метод(методы), определяющий, в каком из данных двух чисел больше цифр.
+ */
+public class Task11 {
+    public static void main(String[] args) {
+
+        int a = 45667;
+        int b = 1234;
+        compareTwoNumbers(getNumberOfDigits(a), a, getNumberOfDigits(b), b);
+    }
+
+    public static int getNumberOfDigits(int a) {
+        int j = 0;
+        for (int i = a; i > 0; i /= 10) {
+            int numerOfDigits = a / i;
+            j++;
+        }
+        return j;
+    }
+
+    public static void compareTwoNumbers(int a, int a1, int b, int b1) {
+        if (a > b) {
+            System.out.println("number of digits in " + a1 + " more");
+        } else if (a < b) {
+            System.out.println("number of digits in " + b1 + " more");
+        } else {
+            System.out.println("same number of digits");
+        }
+    }
+}
